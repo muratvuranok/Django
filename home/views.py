64f0ea4f0b7476b024a285ea5c6bda048778930f
muratvuranok <muratvuranok@hotmail.com>
2019-08-25 11:30:36 +0300

    last version

--- home/views.py
+++ home/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    # return HttpResponse("Hello Fıstklı Baklava")
-
-
-    
+    # return HttpResponse("Hello Fıstklı Baklava") 
     return render(request, 'home/index.html', {'slider':'bilge adam django'})
+
+def contact(request):
+    return render(request,'home/contact.html')
